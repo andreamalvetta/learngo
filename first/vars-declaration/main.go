e6b81cb0580d87609d4bb35f78130063ec05acdf
andreamalvetta <andrea.malvetta@gmail.com>
2019-03-12 17:24:53 +1100

    Vars declarations

---
+++ first/vars-declaration/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var name = "Carl"
+	// var isScientist = true
+	// var age = 62
+	// var degree = 5.
+
+	name := "Carl"
+	isScientist := true
+	age := 62
+	degree := 5.
+
+	fmt.Println(name, isScientist, age, degree)
+}
